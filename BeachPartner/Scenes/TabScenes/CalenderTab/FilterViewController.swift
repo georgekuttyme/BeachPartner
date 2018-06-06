2d988bb460aa10907c2f0ab423dc9fe4d455c989
Powermac <Powermac@Pauls-Mac-mini.local>
2018-06-06 19:21:23 +0530

    filter clearfilter in progress

--- BeachPartner/Scenes/TabScenes/CalenderTab/FilterViewController.swift
+++ BeachPartner/Scenes/TabScenes/CalenderTab/FilterViewController.swift
@@ -277,6 +277,19 @@ class FilterViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     @IBAction func clearAllBtnClicked(_ sender: UIButton) {
+//        self.eventBtn.setTitle("Choose event type", for: UIControlState.normal)
+//        self.eventdropDown.selectRow(0)
+//        self.subTypesBtn.setTitle("Choose subtype", for: UIControlState.normal)
+////        self.subeventsdropDown.selectRow(0)
+//        self.yearBtn.setTitle("Choose year", for: UIControlState.normal)
+//        self.yeardropDown.selectRow(0)
+//        self.monthBtn.setTitle("Choose month", for: UIControlState.normal)
+//        self.monthdropDown.selectRow(0)
+//        self.stateBtn.setTitle("Choose state", for: UIControlState.normal)
+//        self.statedropDown.selectRow(0)
+//        self.regionBtn.setTitle("Choose region", for: UIControlState.normal)
+//        self.regiondropDown.selectRow(0)
+//        filterParams = nil
         delegate?.clearAllFilters()
         //dismiss(animated: true, completion: nil)
     }
