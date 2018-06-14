ab76c7c89d20708421e52231bbb227a1dde33ca7
Powermac <Powermac@Pauls-Mac-mini.local>
2018-06-14 18:32:46 +0530

    up and down arrow changed ,tool bar item color chnaged in feedback,high five lastname added

--- BeachPartner/Scenes/TabScenes/CommmonWebViewController.swift
+++ BeachPartner/Scenes/TabScenes/CommmonWebViewController.swift
@@ -30,7 +30,9 @@ class CommmonWebViewController: UIViewController,UIWebViewDelegate {
         
         let barButtonItemAppearance = UIBarButtonItem.appearance()
         barButtonItemAppearance.setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.white], for: .normal)
-
+        UIPickerView.appearance().tintColor = UIColor.red
+        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UIToolbar.self]).setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor(red: 32.0/255.0, green: 48.0/255.0, blue: 127.0/255.0, alpha: 1.0)], for: .normal)
+//        (red: 41/255.0, green: 56/255.0, blue: 133/255.0, alpha:1.0)
         titleLbl.text = titleText
         if titleText == "About Us"{
             contentType = "https://www.beachpartner.com/about_us.html"
@@ -68,6 +70,7 @@ class CommmonWebViewController: UIViewController,UIWebViewDelegate {
         if dismissActive {
             let barButtonItemAppearance = UIBarButtonItem.appearance()
             barButtonItemAppearance.setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.clear], for: .normal)
+                UIBarButtonItem.appearance(whenContainedInInstancesOf: [UIToolbar.self]).setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.clear], for: .normal)
             super.dismiss(animated: flag, completion: completion)
         }
     }
