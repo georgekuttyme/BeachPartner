8d58fb0ee19f200659a5c06c9e3a3b0b9b69a4e5
Mary <marylis@seqato.com>
2018-08-01 21:22:41 +0530

    ui changes for register

--- BeachPartner/Extensions/BeachPartnerViewController.swift
+++ BeachPartner/Extensions/BeachPartnerViewController.swift
@@ -28,7 +28,7 @@ class BeachPartnerViewController: UIViewController {
         self.dropDown.anchorView = menuButton // UIView or UIBarButtonItem
         // The list of items to display. Can be changed dynamically
         //        self.dropDown.direction = .bottom
-        self.dropDown.dataSource = ["My Profile","Settings","Feedback","Subscription","Add-Ons","Help","Log Out"]
+        self.dropDown.dataSource = ["My Profile","Settings","Feedback","Help","Log Out"]
         
         self.dropDown.bottomOffset = CGPoint(x: 20, y:30)
         self.dropDown.width = 150
