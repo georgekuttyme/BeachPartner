73f881a1f94bdb961ec21491be501e23ab4f494a
Mary <marylis@seqato.com>
2018-09-06 17:59:57 +0530

    resolved #40

--- BeachPartner/Scenes/TabScenes/CalenderTab/FilterViewController.swift
+++ BeachPartner/Scenes/TabScenes/CalenderTab/FilterViewController.swift
@@ -288,6 +288,7 @@ class FilterViewController: UIViewController {
         self.eventBtn.setTitle("Choose event type", for: UIControlState.normal)
         self.eventdropDown.selectRow(0)
         self.subTypesBtn.setTitle("Choose subtype", for: UIControlState.normal)
+        self.subeventsdropDown.clearSelection()
         self.yearBtn.setTitle("2018", for: UIControlState.normal)
         self.yeardropDown.selectRow(0)
         self.monthBtn.setTitle("Choose month", for: UIControlState.normal)
